45f220f76225aa31078169c7070d6fbfd6cf655d
MevlutArslan <mevlut12098@gmail.com>
2024-06-07 13:52:30 -0500

    add custom user button

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,8 +1,43 @@
 import { Button } from "@/components/ui/button";
-import { SignInButton, SignOutButton, UserButton, useAuth } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, UserButton, useAuth, useUser } from "@clerk/nextjs";
+
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+
+const CustomUserButton = () => {
+    const { user } = useUser();
+    const { isSignedIn } = useAuth();
+    const router = useRouter();
+
+    const handleViewAccount = () => router.push('/account');
+
+    if (!isSignedIn) return null;
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost">
+                    <img src={user?.imageUrl} height={20} width={20} alt="user_image" className="rounded-full" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={handleViewAccount}  className="cursor-pointer">
+                    View Account
+                </DropdownMenuItem>
+                <SignOutButton>
+                    <DropdownMenuItem className="cursor-pointer">
+                        Sign Out
+                    </DropdownMenuItem>
+                </SignOutButton>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
+
 
 export const Navbar = () => {
-    const { isSignedIn } = useAuth();
+    const { isSignedIn,  } = useAuth();
     return (
         <nav className="w-full flex items-center justify-between p-4 bg-gray-800 text-white">
             <div className="text-2xl font-bold">My Robot Auction</div>
@@ -12,7 +47,8 @@ export const Navbar = () => {
                 <Button variant="ghost">Contact</Button>
                 {isSignedIn && (
                     <div className="flex items-center">
-                        <UserButton appearance={{ elements: { userButtonAvatarBox: "h-8 w-8" } }} />
+                        <CustomUserButton/>
+                        {/* <UserButton appearance={{ elements: { userButtonAvatarBox: "h-8 w-8" } }} /> */}
                     </div>
                 )}
             </div>
